72f0a1c90eb889d06658c28adc0b9a1351e1fffd
Malik Felix <malikfelix50@gmail.com>
2020-08-27 19:40:00 -0400

    Add Refactored Todo list with added functionality

--- JavaScript/Todo List/script.js
+++ JavaScript/Todo List/script.js
@@ -4,12 +4,31 @@ window.setTimeout(function() {
   while (input !== "quit") {
 
     if (input === "list") {
-      console.log(todos);
+      listTodos();
     } else if(input === "new") {
-      var newTodo = prompt("Enter new todo");
-      todos.push(newTodo);
+      addTodo();
+    } else if(input === "delete") {
+      deleteTodo();
     }
     var input = prompt("What would you like to do?");
   }
   console.log("You just quit the app!");
+
+  function listTodos(){
+    console.log("**********");
+    todos.forEach(function(todo, i) {
+      console.log(i + ": " + todo);
+    });
+    console.log("**********");
+  }
+  function addTodo() {
+    var newTodo = prompt("Enter new todo");
+    todos.push(newTodo);
+    console.log("Added Todo");
+  } 
+  function deleteTodo() {
+    var index = prompt("What is the index number for the item you'd like to delete?");
+      todos.splice(index, 1);
+      console.log("Deleted Todo");
+  }
 }, 500)
