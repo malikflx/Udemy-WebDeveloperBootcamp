f958278181ffc01231ad209dff379d2783859a49
Malik Felix <malikfelix50@gmail.com>
2020-12-23 10:26:03 -0500

    add updates to TV Show Search App

--- APIs/TV Show Search App/app.js
+++ APIs/TV Show Search App/app.js
@@ -2,9 +2,18 @@ const form = document.getElementById('searchForm');
 form.addEventListener('submit', async function (e) {
   e.preventDefault();
   const userInput = form.elements.query.value;
-  const response = await axios.get(`http://api.tvmaze.com/search/shows?q=${userInput}`);
-  console.log();
-  const img = document.createElement('IMG');
-  img.src = response.data[0].show.image.medium;
-  document.body.append(img);
+  const config = { params: { q: userInput } };
+  const response = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+  displayShows(response.data);
+  form.elements.query.value = '';
 })
+
+const displayShows = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement('IMG');
+      img.src = result.show.image.medium;
+      document.body.append(img);
+    }
+  }
+}
